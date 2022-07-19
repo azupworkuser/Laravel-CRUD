318b4ca06e56485a309a64fcbf29153cda43e6b1
Ashish Zarkar <azupworkuser@gmail.com>
2022-07-19 15:07:58 +0530

    Add files via upload

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,46 @@
+<?php return array (
+  'dingo/api' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Dingo\\Api\\Provider\\LaravelServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'API' => 'Dingo\\Api\\Facade\\API',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'tymon/jwt-auth' => 
+  array (
+    'aliases' => 
+    array (
+      'JWTAuth' => 'Tymon\\JWTAuth\\Facades\\JWTAuth',
+      'JWTFactory' => 'Tymon\\JWTAuth\\Facades\\JWTFactory',
+    ),
+    'providers' => 
+    array (
+      0 => 'Tymon\\JWTAuth\\Providers\\LaravelServiceProvider',
+    ),
+  ),
+);
